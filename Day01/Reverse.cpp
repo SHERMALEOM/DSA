cc9fc3435fa3737a5a3adf35ec78e9520319555b
om <shermaleom004@gmail.com>
2026-05-26 11:30:52 +0530

    added new patterns

---
+++ Day01/Reverse.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cin>>num;
+
+    for(int row=1;row<=num;row++){
+
+        for (int space= 0; space <=num-row; space++)
+        {
+          cout<<" ";
+        }
+        
+        for(int col=1;col<=2*row-1;col++){
+            cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
