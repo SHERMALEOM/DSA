55c1296cd6ff233ed82fb3ef21c7195f7e388322
om <shermaleom004@gmail.com>
2026-05-27 20:40:25 +0530

    added new cod of decimal to  binary

--- Day01/Reverse.cpp
+++ Day01/Reverse.cpp
@@ -1,39 +1,18 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int num;
-//     cin>>num;
-
-//     for(int row=1;row<=num;row++){
-
-//         for (int space= 0; space <=num-row; space++)
-//         {
-//           cout<<" ";
-//         }
-        
-//         for(int col=1;col<=2*row-1;col++){
-//             cout<<"*";
-            
-//         }
-//         cout<<endl;
-//     }
-    
-//     return 0;
-// }
-
-
 #include <iostream>
 using namespace std;
 int main() {
     int num;
     cin>>num;
 
-    int count=0;
-    for(int row=0;row<=num;row++){
+    for(int row=1;row<=num;row++){
 
-        for(int numbers=0;numbers<=num;numbers++){count++; 
-
-            cout<<numbers;
+        for (int space= 0; space <=num-row; space++)
+        {
+          cout<<" ";
+        }
+        
+        for(int col=1;col<=2*row-1;col++){
+            cout<<"*";
             
         }
         cout<<endl;
@@ -41,3 +20,5 @@ int main() {
     
     return 0;
 }
+
+
